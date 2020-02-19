7a9967a036f3ce30d27a2e1dfa745bd2d411bd19
cheong <cheong@tester.kaist.ac.kr>
2020-02-19 15:03:42 +0900

    change to end state

--- fuzzer/src/cond_stmt/cond_state.rs
+++ fuzzer/src/cond_stmt/cond_state.rs
@@ -212,6 +212,7 @@ impl NextState for CondStmt {
           rel_list.retain(|x| (x.1 as f64 / target_runs as f64) > config::FUNC_REL_HIGH_THRESHOLD);
         } else {
           rel_list.retain(|x| (x.1 as f64 / target_runs as f64) > config::FUNC_REL_HIGH_THRESHOLD2);
+          self.state = CondState::OffsetAllEnd;
         }
         for (rel_func, _rel) in rel_list {
           let mut rel_cmp_list = func_cmp_map.get(&rel_func).unwrap().clone();
