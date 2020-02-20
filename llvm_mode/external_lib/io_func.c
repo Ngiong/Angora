e8864641648955955fc962a14f9e150beb87c1c9
lockone <dkcjd2000@gmail.com>
2020-02-20 22:17:41 +0900

    getc io func implementation

--- llvm_mode/external_lib/io_func.c
+++ llvm_mode/external_lib/io_func.c
@@ -320,6 +320,12 @@ __dfsw__IO_getc(FILE *fd, dfsan_label fd_label, dfsan_label *ret_label) {
 }
 
 __attribute__((visibility("default"))) int
+__dfsw__getc(FILE *fd, dfsan_label fd_label, dfsan_label *ret_label) {
+  return __dfsw__IO_getc(fd, fd_label, ret_label);
+}
+
+
+__attribute__((visibility("default"))) int
 __dfsw_getchar(dfsan_label *ret_label) {
 
   long offset = ftell(stdin);
