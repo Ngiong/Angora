6986a99a08a44085e62799d412d60c61698bcfb5
Robert Sebastian Herlim <ngiooong@yahoo.com>
2020-08-03 15:57:01 +0900

    working instrumentation for argc argv

--- runtime_fast/src/context.c
+++ runtime_fast/src/context.c
@@ -1,5 +1,7 @@
 
 #include "stdint.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 // uint32_t __angora_cond_cmpid;
 // void __angora_set_cmpid(uint32_t id) { __angora_cond_cmpid = id; }
@@ -29,7 +31,83 @@ int __print_argc_argv(int argc, char *argv[]) {
 }
 
 int main(int argc, char *argv[]) {
-  int tmp_argc = argc;
-  char **tmp_argv = argv;
-  return __print_argc_argv(tmp_argc, tmp_argv);
+  if (argc < 2) {
+    perror("Argc & Argv canno be less than 2.");
+    return 1;
+  }
+
+  FILE *raw = fopen(argv[1], "rb");
+  if (raw == NULL) {
+    perror("Cannot open file argv[1]");
+    return 1;
+  }
+
+  char new_name[100];
+  strcpy(new_name, argv[1]);
+  strcat(new_name, "p");
+
+  FILE *result = fopen(new_name, "wb");
+  if (result == NULL) {
+    perror("Cannot open file to write output");
+    exit(1);
+  }
+
+  int c, prev;
+  int buffer[1000], i = 0, j = 0;
+  int get_opt_state = 1;
+  while ((c = fgetc(raw)) != EOF) {
+    if (get_opt_state) {
+      if (i > 0 && prev == '\0' && c == '\0') {
+        get_opt_state = 0;
+      } else {
+        buffer[i++] = c;
+      }
+    } else {
+      fputc(c, result);
+    }
+    prev = c;
+  }
+
+  fclose(raw);
+  fclose(result);
+
+  unsigned char uc_buffer[1000];
+  int len = i;
+  for (i = 0; i < len; i++) {
+    uc_buffer[i] = buffer[i];
+  }
+
+  // count how many opt in the buffer
+  i = 0;
+  int in_flag = 1, count_opt = 0;
+  int new_argc = 0;
+  char *new_argv[1000];
+  new_argv[new_argc++] = argv[0];
+
+  // make sure we start in_flag (non-whitespace character)
+  while (uc_buffer[i] == ' ') i++;
+  new_argv[new_argc++] = uc_buffer + i;
+  
+  for (; i < len; i++) {
+    int is_delimiter = uc_buffer[i] == ' ' || uc_buffer[i] == '\0';
+    if (in_flag == 1) {
+      if (is_delimiter) {
+        uc_buffer[i] = '\0';
+        in_flag = 0;
+        count_opt++;
+      } else {
+        // do nothing
+      }
+    } else {
+      if (!is_delimiter) {
+        new_argv[new_argc++] = uc_buffer + i;
+        in_flag = 1;
+      } else {
+        // do nothing
+      }
+    }
+  }
+
+  new_argv[new_argc++] = new_name;
+  return __print_argc_argv(new_argc, new_argv);
 }
